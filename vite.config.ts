9cdb20a0c7e36643c5edcfab3e430c386fb223af
Nerdsus-afk <yashwantec36@gmail.com>
2026-05-11 14:53:40 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/portfolio/',
+  base: '/flourish-portfolio-kit-e39aa935/',
   server: {
     host: "::",
     port: 8080,
